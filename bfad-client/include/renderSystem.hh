bfd15334d76903cbed676327a4c5fc7173686db7
Blackoutburst <github@blackoutburst.dev>
2026-03-11 15:29:48 +0100

    added missing render system files

---
+++ bfad-client/include/renderSystem.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include "types.hh"
+#include "context.hh"
+
+#include "renderer/swapchain.hh"
+#include "renderer/imageView.hh"
+
+namespace RenderSystem {
+    struct It {
+        VkSwapchainKHR swapchain;
+        ImageView::It* imageView;
+        VkFramebuffer* framebuffers;
+        VkRenderPass renderPass;
+    };
+
+    RenderSystem::It* create(Context::It* ctx);
+    U0 destroy(Context::It* ctx, RenderSystem::It* renderSystem);
+    U0 update(Context::It* ctx, RenderSystem::It* renderSystem);
+}
---
+++ bfad-client/src/renderSystem.cc
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+#include "renderSystem.hh"
+
+#include "renderer/framebuffer.hh"
+#include "renderer/renderPass.hh"
+
+namespace RenderSystem {
+    RenderSystem::It* create(Context::It* ctx) {
+        VkSwapchainKHR swapchain = Swapchain::create(ctx);
+        ImageView::It* imageView = ImageView::create(ctx, swapchain);
+        VkRenderPass renderPass = RenderPass::create(ctx);
+        VkFramebuffer* framebuffers = Framebuffer::create(ctx, swapchain, renderPass, imageView->imageView);
+    
+        RenderSystem::It* renderSystem = (RenderSystem::It*)malloc(sizeof(RenderSystem::It));
+
+        renderSystem->swapchain = swapchain;
+        renderSystem->imageView = imageView;
+        renderSystem->framebuffers = framebuffers;
+        renderSystem->renderPass = renderPass;
+
+        return renderSystem;
+    }
+    
+    U0 destroy(Context::It* ctx, RenderSystem::It* renderSystem) {
+        Framebuffer::destroy(ctx, renderSystem->swapchain, renderSystem->framebuffers);
+        RenderPass::destroy(ctx, renderSystem->renderPass);
+        ImageView::destroy(ctx, renderSystem->imageView);
+        Swapchain::destroy(ctx, renderSystem->swapchain);
+    
+        free(renderSystem);
+    }
+
+    U0 update(Context::It* ctx, RenderSystem::It* renderSystem) {
+        vkDeviceWaitIdle(ctx->device->logical);
+
+        Framebuffer::destroy(ctx, renderSystem->swapchain, renderSystem->framebuffers);
+        ImageView::destroy(ctx, renderSystem->imageView);
+        Swapchain::destroy(ctx, renderSystem->swapchain);
+
+        VkSwapchainKHR swapchain = Swapchain::create(ctx);
+        ImageView::It* imageView = ImageView::create(ctx, swapchain);
+        VkFramebuffer* framebuffers = Framebuffer::create(ctx, swapchain, renderSystem->renderPass, imageView->imageView);
+
+        renderSystem->swapchain = swapchain;
+        renderSystem->imageView = imageView;
+        renderSystem->framebuffers = framebuffers;
+    }
+}
