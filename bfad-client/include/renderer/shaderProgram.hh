db40e9f53759d56d833b846d2038b0caffcea994
Blackoutburst <github@blackoutburst.dev>
2026-03-12 09:10:22 +0100

    added missing shader program files

---
+++ bfad-client/include/renderer/shaderProgram.hh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include "context.hh"
+#include "types.hh"
+
+namespace ShaderProgram {
+    struct It {
+        VkPipelineShaderStageCreateInfo* handle;
+        VkShaderModule vertexShader;
+        VkShaderModule fragmentShader;
+    };
+
+    VkPipelineShaderStageCreateInfo createShaderInfo(VkShaderStageFlagBits stage, VkShaderModule shader);
+    ShaderProgram::It* create(Context::It* ctx, const I8* vertexSource, const I8* fragmentSource);
+    U0 destroy(Context::It* ctx, ShaderProgram::It* shaderProgram);
+}
